c949bc0b0f6c6e820aff470c6ef1c4b213b4bf20
AlbertKNguyen <43667021+AlbertKNguyen@users.noreply.github.com>
2019-10-27 21:42:24 -0700

    comment out prints

--- threads/Boat.java
+++ threads/Boat.java
@@ -9,7 +9,7 @@ public class Boat
     public static void selfTest()
     {
 		BoatGrader b = new BoatGrader();
-		
+
 		System.out.println("\n ***Testing Boats with only 2 children***");
 		begin(0, 2, b);
 
@@ -45,7 +45,6 @@ public class Boat
 
 		System.out.println("\n ***Testing Boats with 50 children, 37 adults***");
 		begin(37, 50, b);
-
 	}
 	
 	enum Location
@@ -110,7 +109,7 @@ public class Boat
 		}
 
 		int numberOfMolokaiPeople = communicator.listen();
-		System.out.println("All " + numberOfMolokaiPeople + " of the children and adults are all now on Molokai.");
+		// System.out.println("All " + numberOfMolokaiPeople + " of the children and adults are all now on Molokai.");
     }
 
     static void AdultItinerary(Location adultLocation)
@@ -192,9 +191,8 @@ public class Boat
 						waitOnMolokai.sleep();
 					} else {
 						// Report back the total number of adults and children on Molokai and end of simulation
-						System.out.println("Adults: " + numberOfMolokaiAdults);
-						System.out.println("Children: " + numberOfMolokaiChildren);
-						System.out.println(numberOfMolokaiAdults + numberOfMolokaiChildren);
+						//System.out.println("Adults: " + numberOfMolokaiAdults);
+						//System.out.println("Children: " + numberOfMolokaiChildren);
 						communicator.speak(numberOfMolokaiAdults + numberOfMolokaiChildren);
 					}
 				} else if (numberOfOahuChildren == 1) {
